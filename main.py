87d2850ded9e1a224f773db8919328c5247a4229
BassSwing <saigin.mkhl@gmail.com>
2021-11-21 00:38:13 +0500

    Latest commit

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
-def print_hi(name):
-    print(f'Hi, {name}')
-    print('abracadabra')
-    print('1')
-    print('2')
-    print('3')
+import sys
+import random
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QPainter, QColor, QFont, QPen
+from PyQt5.QtCore import Qt
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Ellipses')
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, e):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        lst = [random.randint(-300, 300) for i in range(10)]
+        # Задаем кисть
+        qp.setBrush(QColor(255, 255, 0))
+        # Рисуем прямоугольник заданной кистью
+        for i in range(10):
+            x = random.randint(20, 400)
+            y = random.randint(20, 400)
+            qp.drawEllipse(x, y, 120, 120)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
